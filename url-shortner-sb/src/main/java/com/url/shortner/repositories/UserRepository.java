c2be2e23c7be99082308876ec141ad4038df19db
AkshayMohan <akshay.mohan@genworkshealth.com>
2025-06-18 07:59:05 +0530

    Url Shortening Functionality Completed.

--- url-shortner-sb/src/main/java/com/url/shortner/repositories/UserRepository.java
+++ url-shortner-sb/src/main/java/com/url/shortner/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.url.shortner.repositories;
 
 import com.url.shortner.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
---
+++ url-shortner-sb/src/main/java/com/url/shortner/service/UrlMappingService.java
@@ -0,0 +1,56 @@
+package com.url.shortner.service;
+
+import com.url.shortner.dto.UrlMappingDTO;
+import com.url.shortner.models.UrlMapping;
+import com.url.shortner.models.User;
+import com.url.shortner.repositories.UrlMappingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class UrlMappingService {
+    @Autowired
+    private UrlMappingRepository urlMappingRepository;
+
+    public List<UrlMappingDTO> getUrlsByUser(User user) {
+        return urlMappingRepository.findByUser(user).stream()
+                .map(this::convertToDto)
+                .toList();
+    }
+
+    public UrlMappingDTO createShortUrl(String originalUrl, User user) {
+        String shortUrl = generateShortUrl();
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setUser(user);
+        urlMapping.setCreatedDate(LocalDateTime.now());
+        urlMapping.setShortUrl(shortUrl);
+        UrlMapping savedUrlMapping = urlMappingRepository.save(urlMapping);
+        return convertToDto(savedUrlMapping);
+    }
+
+    private UrlMappingDTO convertToDto(UrlMapping urlMapping){
+        UrlMappingDTO urlMappingDTO = new UrlMappingDTO();
+        urlMappingDTO.setId(urlMapping.getId());
+        urlMappingDTO.setOriginalUrl(urlMapping.getOriginalUrl());
+        urlMappingDTO.setShortUrl(urlMapping.getShortUrl());
+        urlMappingDTO.setCreatedDate(urlMapping.getCreatedDate());
+        urlMappingDTO.setClickCount(urlMapping.getClickCount());
+        urlMappingDTO.setUsername(urlMapping.getUser().getUsername());
+        return urlMappingDTO;
+    }
+
+    private String generateShortUrl() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder shortUrl = new StringBuilder(8);
+        for (int i = 0; i < 8; i++) {
+            shortUrl.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return shortUrl.toString();
+    }
+}
